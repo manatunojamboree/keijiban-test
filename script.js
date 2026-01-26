24de6a16f469e88e1988785fb7591ac3d8a017ce
manatunojamboree <itsorengetime@gmail.com>
2026-01-26 20:21:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const board = document.getElementById("board");
 
-fetch("https://script.google.com/a/macros/ome-tky.ed.jp/s/AKfycbzLxpj5sUVrpEylAY_YRumIZi1bcEOyFdbKYn7z_3oA6Y3B2TLYmnk10JEpUSeFtHNj/exec")
+fetch("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLh2rWXTrInpyg0Ll49V_tz62dFzgXy8-SdgbNZri10OdLQ1QsDZx1dWzyTAAA50TnRUPjfG4aIPvWGpd-zMv_gSTze9rD-uElwe_iEhcZ-C57TWZZfQsg-2VLv5JWtGCTAOwu7tFW_TJ5Qgzcw5TaGrwmd58Fh-dXB8D46uZiq4RfJ_H9K2CT7UNC63HtQgACJRejkVv6HOvm36u2C36t3geUudtHZcPcPk5k-9hbDR7uRKSJCP36CLJ4VdF205p7qcH_Eu5eFPmN1leNw22VyQBcv6Bw&lib=Mh0cYyEBxgD42cU-LfNC2iACKPjFc87IZ")
   .then(res => res.json())
   .then(posts => {
     board.innerHTML = "";
